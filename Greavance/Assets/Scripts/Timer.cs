6f5af8576efac167c10db88218fa8a802205db52
peopleNo1 <jasminechao96722@gmail.com>
2026-04-03 00:47:21 -0400

    Adjusting level 1 and add a timer
    - changed camera size to 4
    - timer added: when enter the gamePlay scene start, open the guide will pause, close guide continue
    level 1
    - adjust key size
    - change door color block sprite to key
    - edit collider for rocks

---
+++ Greavance/Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] Text text;
+    bool isStop;
+    bool done = true;
+    int time;
+
+    void Update()
+    {
+        if (done)
+            StartCoroutine(CountUp());
+    }
+
+    public void ResetTimer()
+    {
+        time = 0;
+        Pause();
+    }
+
+    public bool GetIsStop()
+    {
+        return isStop;
+    }
+
+    public IEnumerator CountUp()
+    {
+        done = false;
+        time++;
+        text.text = "Timer: " + time;
+
+        yield return new WaitForSeconds(1);
+        done = true;
+    }
+
+    public void Pause()
+    {
+        isStop = true;
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        isStop = false;
+        Time.timeScale = 1;
+    }
+}
