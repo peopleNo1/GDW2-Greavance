94c508a0665a97baf38811cb4c059178e1c0c130
DudsMBF <eduardomarcelo.defreitas@ontariotechu.net>
2026-04-04 17:00:34 -0400

    Boss attack fixed

--- Greavance/Assets/Scripts/Enemy/Boss/BossBasicAttack.cs
+++ Greavance/Assets/Scripts/Enemy/Boss/BossBasicAttack.cs
@@ -6,34 +6,28 @@ public class BossBasicAttacks : MonoBehaviour
 {
     [SerializeField] public float _damage = 10.0f;
     [SerializeField] public float _speed = 3f;
-    PlayerController _player;
+
     private Vector2 _direction = Vector2.down;
 
     private void Update()
     {
         transform.Translate(_direction * _speed * Time.deltaTime);
-        _player = GetComponent<PlayerController>();
     }
 
-
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Boss")) {return;}
             
         if (collision.gameObject.CompareTag("Player"))
         {
-            _player.TakeDamage(_damage);
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.TakeDamage(_damage);
             Destroy(gameObject);
         }
-        else if (collision.gameObject.CompareTag("Boss") || 
-                collision.gameObject.CompareTag("Enemy"))
+        else if (collision.gameObject.CompareTag("Boss") || collision.gameObject.CompareTag("Enemy"))
         {
-            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+            Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>());
             return;
-        }
-        else
-        {
-            Destroy(gameObject);
         }
     }
 }
